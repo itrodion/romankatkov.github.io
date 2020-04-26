166c6d4c42b6af7970637b77ef190d5d9d88e3f9
itrodion <64366268+itrodion@users.noreply.github.com>
2020-04-26 20:07:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,278 @@
+//
+//
+//
+//Кнопки навигации
+//
+//
+//
+function show(id)
+{
+	document.getElementById('methodCaesar').style.display = 'block'
+	document.getElementById('esar').style.display = 'none'
+	document.getElementById('ssa').style.background = 'black'
+	document.getElementById('sss').style.background = '#454547'
+	document.getElementById('saa').style.background = '#454547'
+
+}
+function sio(id){
+	document.getElementById('sss').style.background = '#454547'
+	document.getElementById('ssa').style.background = '#454547'
+	document.getElementById('saa').style.background = 'black'
+	document.getElementById('esar').style.display = 'none'
+	document.getElementById('methodCaesar').style.display = 'none'
+
+}
+function sho(id)
+{
+	document.getElementById('sss').style.background = 'black'
+	document.getElementById('sss').style.background = 'black'
+	document.getElementById('saa').style.background = '#454547'
+	document.getElementById('ssa').style.background = '#454547'
+	document.getElementById('esar').style.display = 'block'
+	document.getElementById('methodCaesar').style.display = 'none'
+
+}
+//
+//
+//
+//Kнопки Операций
+//
+//
+//
+function barb(id)
+{	
+	document.getElementById('VID1').style.display = 'block'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('barbarosa').style.display = 'block'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function t(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'block'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('tif').style.display = 'block'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function y(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'block'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'block'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function k(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'block'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function isk(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'block'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('iskra').style.display = 'block'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'block'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function cit(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'block'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'block'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function kut(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'block'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'block'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function bag(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'block'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'block'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'none'
+}
+function vis(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'block'
+	document.getElementById('VID9').style.display = 'none'
+
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'block'
+	document.getElementById('berl').style.display = 'none'
+}
+function b(id)
+{
+	document.getElementById('VID1').style.display = 'none'
+	document.getElementById('VID2').style.display = 'none'
+	document.getElementById('VID3').style.display = 'none'
+	document.getElementById('VID4').style.display = 'none'
+	document.getElementById('VID5').style.display = 'none'
+	document.getElementById('VID6').style.display = 'none'
+	document.getElementById('VID7').style.display = 'none'
+	document.getElementById('VID8').style.display = 'none'
+	document.getElementById('VID9').style.display = 'block'
+
+	document.getElementById('berl').style.display = 'none'
+	document.getElementById('barbarosa').style.display = 'none'
+	document.getElementById('tif').style.display = 'none'
+	document.getElementById('yran').style.display = 'none'
+	document.getElementById('kolco').style.display = 'none'
+	document.getElementById('iskra').style.display = 'none'
+	document.getElementById('citad').style.display = 'none'
+	document.getElementById('kutuzov').style.display = 'none'
+	document.getElementById('bagrat').style.display = 'none'
+	document.getElementById('vislo').style.display = 'none'
+	document.getElementById('berl').style.display = 'block'
+}
